9b9aedf00125a0b63cc71826c84340643140f1b2
Rob Wormald <rob.wormald@innitapps.com>
2014-07-10 23:15:47 -0500

    more travis

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,23 @@
-var sharedConfig = require('pipe/karma');
-
 module.exports = function(config) {
-  sharedConfig(config);
-  config.traceurPreprocessor.options.asyncFunctions = true;
-
   config.set({
-    // list of files / patterns to load in the browser
+    frameworks: ['jasmine', 'traceur', 'requirejs'],
+
+    traceurPreprocessor: {
+      options: {
+        modules: 'amd',
+        types: true,
+        typeAssertions: true,
+        typeAssertionModule: 'assert',
+        annotations: true,
+        sourceMap: true
+        /**
+         * Someday blockBinding would be nice, but for now it compiles to nasty
+         * ES5 code (everything in a closure)
+         */
+        // blockBinding: true
+      }
+    },
+
     files: [
       'node_modules/traceur/bin/traceur.js',
       'test-main.js',
@@ -40,9 +52,49 @@ module.exports = function(config) {
       accessKey: 'ff7a06d5-2844-4d69-a244-c783894155ed',
       startConnect: true,
       testName: 'innit_core_data'
-    }
+    },
+    customLaunchers: {
+      'Chrome_harmony': {
+        base: 'Chrome',
+        flags: ['--js-flags=--harmony']
+      },
 
-});
+      // Sauce Labs browsers
+      'SL_Chrome': {
+        base: 'SauceLabs',
+        browserName: 'chrome'
+      },
+      'SL_Firefox': {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        version: '26'
+      },
+      'SL_Safari': {
+        base: 'SauceLabs',
+        browserName: 'safari',
+        platform: 'OS X 10.9',
+        version: '7'
+      }
+    },
 
 
+    browsers: ['Chrome'],
+
+
+    plugins: [
+      'karma-*'
+      // require('karma-sauce-launcher')
+    ]
+  });
+
+  if (process.env.TRAVIS) {
+    config.sauceLabs.build = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
+    config.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
+
+    process.env.SAUCE_ACCESS_KEY = process.env.SAUCE_ACCESS_KEY.split('').reverse().join('');
+
+    // TODO(vojta): remove once SauceLabs supports websockets.
+    // This speeds up the capturing a bit, as browsers don't even try to use websocket.
+    config.transports = ['xhr-polling'];
+  }
 };
